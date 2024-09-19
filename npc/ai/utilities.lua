17f61fc463dd6952bce5b7a1d483a39155c11100
Allie Zhao <allie.k.zhao@gmail.com>
2024-09-18 19:10:58 -0700

    Create new "ai utility functions" module

---
+++ npc/ai/utilities.lua
@@ -0,0 +1,40 @@
+-- Module for functions commonly used in NPC AI processing.
+
+local M = {}
+
+-- Check if the player can be seen from the NPC's body position (i.e. is the line between player and NPC unobstructed)
+-- Returns false if any props or terrain are between NPC and player body positions, true otherwise
+M.canSeePlayer = function(thisNpc, world, playerObj)
+  local closestTerrainFract = 1
+  local closestPropFract = 1
+  local playerFract = 0
+  -- this callback func is called during raycast, when the ray hits any fixture
+  -- THE CALLBACKS AREN'T ORDERED, the ray could hit a farther fixture before a closer one
+  -- That's why we're tracking distance values for the closest prop/terrain, to compare with player distance
+  local rayHitCallback = function(fixture, x, y, xn, yn, fraction)
+    if fixture:getUserData().type == "terrain" then
+      if closestTerrainFract > fraction then closestTerrainFract = fraction end
+      return -1
+    end
+    if fixture:getUserData().type == "prop" then
+      closestPropFract = fraction
+      return -1
+    end
+    if fixture:getUserData().type == "player_hardbox" then
+      playerFract = fraction
+      return -1
+    end
+    return -1
+  end
+  -- cast the ray, feeding it our callback
+  world:rayCast(thisNpc:getX(), thisNpc:getY(), playerObj.getX(), playerObj.getY(), rayHitCallback)
+  -- check fraction values of what we hit
+  if playerFract > closestTerrainFract or playerFract > closestPropFract then
+    return false
+  else
+    return true
+  end
+end
+
+return M
+-- vim: foldmethod=marker
--- npc/enemydefs/lazyTurret.lua
+++ npc/enemydefs/lazyTurret.lua
@@ -1,4 +1,5 @@
 local gunlib = require("guns")
+local aiUtils = require("npc.ai.utilities")
 
 local M = {}
 
@@ -34,28 +35,12 @@ M.aiCycle = function(self, world, playerObj, npcList)
   -- this enemy:
   --   - only shoots if it has a clear shot to the player
   --   - shoots once every update where it has a clear shot
-  local terrainFract = 1
-  local playerFract = 0
-  -- this callback func is called during raycast, when the ray hits any fixture
-  -- BTW THE CALLBACKS AREN'T ORDERED, the ray could hit a farther fixture before a closer one
-  local rayHitCallback = function(fixture, x, y, xn, yn, fraction)
-    if fixture:getUserData().type == "terrain" then
-      if terrainFract > fraction then terrainFract = fraction end
-      return -1
-    end
-    if fixture:getUserData().type == "player_hardbox" then
-      playerFract = fraction
-      return -1
-    end
-    return -1
-  end
-  -- cast a ray to see if there's any shot-blocking terrain in the way
-  world:rayCast(self:getX(), self:getY(), playerObj.getX(), playerObj.getY(), rayHitCallback)
-  -- if we have an unobstructed shot, shoot your gun
-  if playerFract > terrainFract then
-    print("no see u :(")
-  else
+
+  -- check if we can see the player
+  if aiUtils.canSeePlayer(self, world, playerObj) then
     print("i see uu!!!!")
+  else
+    print("no see u :(")
   end
 end
 
